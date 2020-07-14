09068e9f744602f8f18b5b830b4a4c0daa28f53f
Yorick Smilda <y.smilda@gmail.com>
2020-07-14 20:31:00 +0200

    rework for websockets

--- Receiver/receiver.ino
+++ Receiver/receiver.ino
@@ -1,63 +1,211 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
-#include <ArduinoJson.h> // Include the Arduino JSON parser library.
+#include <ArduinoWebsockets.h>
+#include <FS.h>
 #include <Servo.h>
 
-const char *ssid = "Robothand";       // The SSID (name) of the Wi-Fi network you want to create
-const char *password = "VeryRobotic"; // The password of the Wi-Fi network
+char ssid[] = "IoT"; // your network SSID (name)
 
+using namespace websockets;
+
+WebsocketsServer wsServer;
+WebsocketsClient client;
 ESP8266WebServer server(80);
 
-int fingers_positions[5];
+Servo thumb;
+Servo index_finger;
+Servo middle_finger;
+Servo ring_finger;
+Servo pinky;
 
-Servo finger_1;
+int finger_positions[5];
 
 void setup()
 {
   Serial.begin(115200);
-  delay(10);
-  Serial.println('\n');
 
-  WiFi.softAP(ssid, password, 6, true); // Start an accespoint at channel 6 with a hidden ssid.
-  Serial.print("Access Point \"");
-  Serial.print(ssid);
-  Serial.println("\" started");
+  WiFi.begin(ssid);
+
+  while (WiFi.status() != WL_CONNECTED)
+  { // Wait for the Wi-Fi to connect
+    delay(250);
+  }
 
   Serial.print("IP address:\t");
-  Serial.println(WiFi.softAPIP()); // Send the IP address of the ESP8266 to the computer
+  Serial.println(WiFi.localIP()); // Send the IP address of the ESP8266 to the computer
 
-  server.onNotFound([]() {
-    server.send(404, "text/plain", "404: Not Found"); // respond with a 404 (Not Found) error if the requested URI doesn't exist.
+  // start the Websocket server on port 8080.
+  wsServer.listen(8080);
+
+  SPIFFS.begin(); // Start the SPI Flash Files System.
+
+  server.onNotFound([]() {                              // If the client requests any URI.
+    if (!handle_file_read(server.uri()))                // send it if it exists.
+      server.send(404, "text/plain", "404: Not Found"); // otherwise, respond with a 404 (Not Found) error.
   });
 
-  server.on("/fingers", HTTP_POST, handle_post);
-
+  // Start the server on port 80.
   server.begin();
 
-  finger_1.attach(14);
+  thumb.attach(16);
+  index_finger.attach(4);
+  middle_finger.attach(14);
+  ring_finger.attach(12);
+  pinky.attach(13);
 }
 
 void loop()
 {
   server.handleClient();
 
-  uint16_t temp = map(fingers_positions[0], 0, 900, 180, 80);
-  finger_1.write(temp);
+  if (wsServer.poll()) // Check if there is a new client trying to connect.
+  {
+    Serial.println("New client");
+    client = wsServer.accept();
+
+    // Setup Callbacks for the new client.
+    client.onMessage(onMessageCallback);
+    client.onEvent(onEventsCallback);
+  }
+
+  if (client.available()) // If there is a client check if it has new messages.
+  {
+    client.poll();
+  }
 }
 
-void handle_post()
+// Functions to handle websocket messages and events
+
+void onEventsCallback(WebsocketsEvent event, String data)
 {
-  String json = server.arg("plain");
-  Serial.println(json);
+  switch (event)
+  {
+  case WebsocketsEvent::ConnectionOpened:
+    Serial.println("Connnection Opened");
+    break;
 
-  uint8_t comma_index = json.indexOf(",");
+  case WebsocketsEvent::ConnectionClosed:
+    Serial.println("Connnection Closed");
+    client.close();
+    break;
+  }
+}
 
-  fingers_positions[0] = json.substring(1, comma_index).toInt(); // 1
-  Serial.println(fingers_positions[0]);
-  // fingers_positions[1] = data[1]; // 2
-  // fingers_positions[2] = data[2]; // 3
-  // fingers_positions[3] = data[3]; // 4
-  // fingers_positions[4] = data[4]; // 5
+void onMessageCallback(WebsocketsMessage message)
+{
+  Serial.print("Got Message: ");
+  Serial.println(message.data());
+  String data = message.data();
 
-  server.send(202);
+  switch (data[0]) // Parse the incoming message.
+  {
+  case 'f':
+    unmarshall_finger_positions(data.substring(2));
+    update_finger_positions();
+    break;
+
+  case 'i':
+    data = marshall_finger_positions();
+    client.send(data);
+    break;
+  }
+}
+
+// Functions to handle the webserver file serving
+
+String get_content_type(String filename)
+{ // Convert the file extension to the correct MIME type.
+  if (filename.endsWith(".html"))
+  {
+    return "text/html";
+  }
+  else if (filename.endsWith(".css"))
+  {
+    return "text/css";
+  }
+  else if (filename.endsWith(".js"))
+  {
+    return "application/javascript";
+  }
+  else if (filename.endsWith(".ico"))
+  {
+    return "image/x-icon";
+  }
+  return "text/plain";
+}
+
+bool handle_file_read(String path)
+{ // Send the correct file to the client (if it exists).
+  if (path.endsWith("/"))
+  {
+    path += "index.html"; // If a folder is requested, send the index file.
+  }
+
+  String contentType = get_content_type(path); // Get the MIME type.
+
+  if (SPIFFS.exists(path))
+  {                                                     // If the file exists.
+    File file = SPIFFS.open(path, "r");                 // Open it.
+    size_t sent = server.streamFile(file, contentType); // And send it to the client.
+    file.close();                                       // Then close the file again.
+    return true;
+  }
+
+  return false; // If the file doesn't exist, return false.
+}
+
+void unmarshall_finger_positions(String message)
+{
+  uint8_t index = 0;
+  uint8_t next_index = message.indexOf(",");
+
+  finger_positions[0] = message.substring(index, next_index).toInt();
+
+  index = next_index + 1;
+  next_index = message.indexOf(",", index);
+
+  finger_positions[1] = message.substring(index, next_index).toInt();
+
+  index = next_index + 1;
+  next_index = message.indexOf(",", index);
+
+  finger_positions[2] = message.substring(index, next_index).toInt();
+
+  index = next_index + 1;
+  next_index = message.indexOf(",", index);
+
+  finger_positions[3] = message.substring(index, next_index).toInt();
+
+  finger_positions[4] = message.substring(next_index + 1).toInt();
+}
+
+String marshall_finger_positions()
+{
+  String data = "f,";
+
+  data += finger_positions[0];
+  data += ",";
+  data += finger_positions[1];
+  data += ",";
+  data += finger_positions[2];
+  data += ",";
+  data += finger_positions[3];
+  data += ",";
+  data += finger_positions[4];
+
+  return data;
+}
+
+void update_finger_positions(void)
+{
+  thumb.write(adc_to_rotation(finger_positions[0], 80, 180));
+  index_finger.write(adc_to_rotation(finger_positions[1], 60, 180));
+  middle_finger.write(adc_to_rotation(finger_positions[2], 60, 180));
+  ring_finger.write(adc_to_rotation(finger_positions[3], 60, 180));
+  pinky.write(adc_to_rotation(finger_positions[4], 60, 180));
+}
+
+int adc_to_rotation(int value, int min, int max)
+{
+  return value * (max - min) / 4095.00 + min; // 4095 is the maximum ADC value.
 }
--- Sender/sender.ino
+++ Sender/sender.ino
@@ -1,75 +1,75 @@
 #include <ESP8266WiFi.h>
-#include <ESP8266HTTPClient.h>
-#include <ArduinoJson.h> // Include the Arduino JSON parser library.
+#include <ArduinoWebsockets.h>
 #include <Wire.h>
+#include <ADC128D818.h>
 
-const char *ssid = "Robothand";       // The SSID (name) of the Wi-Fi network you want to connect to
-const char *password = "VeryRobotic"; // The password of the Wi-Fi network
-const char *serverName = "http://192.168.4.1/fingers";
+const char *ssid = "IoT"; // The SSID (name) of the Wi-Fi network you want to connect to
+const char *websockets_server = "ws://192.168.2.22:8080";
+
+using namespace websockets;
+
+WebsocketsClient client;
+
+ADC128 adc(0x1D);
 
 void setup()
 {
-    Wire.begin();
     Serial.begin(115200);
-    delay(10);
 
-    WiFi.begin(ssid, password); // Connect to the network
+    WiFi.begin(ssid); // Connect to the network
     Serial.print("Connecting to ");
     Serial.println(ssid);
 
-    int i = 0;
     while (WiFi.status() != WL_CONNECTED)
     { // Wait for the Wi-Fi to connect
         delay(250);
-        Serial.print('.');
     }
-    Serial.println();
 
     Serial.println();
     Serial.println(WiFi.localIP());
+
+    Wire.begin();
+    adc.reset();
+
+    ADC128_advanced_configuration advanced_configuration;
+    advanced_configuration.selected_mode = 1;
+
+    adc.write_advanced_configuration(advanced_configuration);
+
+    ADC128_channels disabled_channels; // A channel which is true is disabled
+    disabled_channels.channel_0 = true;
+    disabled_channels.channel_1 = true;
+    disabled_channels.channel_2 = true;
+
+    adc.write_disabled_channels(disabled_channels);
+
+    adc.set_conversion_rate(CONTINUOUS);
+    adc.start();
+
+    // Connect to server
+    client.connect(websockets_server);
 }
 
 void loop()
 {
-    int j = 0;
-    unsigned long last = millis();
-    while ((millis() - last) < 1000)
-    {
-        Wire.requestFrom(0x18, 10);
+    String data = marshall_message();
 
-        uint8_t readings[10];
-        uint8_t i = 0;
+    client.send(data);
+}
 
-        while (Wire.available()) // slave may send less than requested
-        {
-            readings[i++] = Wire.read();
-        }
+String marshall_message()
+{
+    String data = "f,";
 
-        HTTPClient http;
+    data += adc.read_ADC_channel(7);
+    data += ",";
+    data += adc.read_ADC_channel(6);
+    data += ",";
+    data += adc.read_ADC_channel(5);
+    data += ",";
+    data += adc.read_ADC_channel(4);
+    data += ",";
+    data += adc.read_ADC_channel(3);
 
-        http.begin(serverName);
-        http.addHeader("content-type", "text/plain");
-
-        DynamicJsonDocument doc(100);
-
-        JsonArray data = doc.createNestedArray("f"); // Create a JSON object.
-        data.add((uint16_t)(readings[1] << 8) + readings[0]);        // Add all the measurements.
-        data.add((uint16_t)(readings[3] << 8) + readings[2]);
-        data.add((uint16_t)(readings[5] << 8) + readings[4]);
-        data.add((uint16_t)(readings[7] << 8) + readings[6]);
-        data.add((uint16_t)(readings[9] << 8) + readings[8]);
-
-        String JSON;
-        serializeJson(data, JSON); // Serialize the JSON object into a String.
-        // Serial.println(JSON);
-
-        int response = http.POST(JSON); // Post the HTTP message.
-
-        http.end(); // End the HTTP client.
-
-        j++;
-    }
-
-    Serial.print("Number of requests done in 1 second: ");
-    Serial.println(j);
+    return data;
 }
